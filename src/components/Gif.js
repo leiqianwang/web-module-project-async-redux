a7c7a77c267c4d380adb1eed052624fbc1c176ed
Yingying Wang <yxw241@gmail.com>
2023-12-20 18:20:53 -0500

    3rd draft

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,6 +1,21 @@
 import React from "react";
 
+const Gif = props => {
+    const { gif } = props;
 
+
+    return (
+        <div class="gif" style="width: 18rem;">
+            <h2>{gif.title}</h2>
+            <p>{gif.username}</p>
+            <img width="300" src={gif.images.original.url} />
+            <div class="gif-body">
+                <p>Rating: <strong>{gif.rating}</strong></p>
+                <a target="_blank" href={gif.url}>Check it out on Giphy</a>
+            </div>
+        </div>
+    )
+}
 
 
 

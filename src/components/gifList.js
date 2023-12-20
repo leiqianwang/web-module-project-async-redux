495d5683ba10a4b274c6a3a891b8c2a69f783a5e
Yingying Wang <yxw241@gmail.com>
2023-12-20 18:10:45 -0500

    second init draft

--- src/components/gifList.js
+++ src/components/gifList.js
@@ -1,1 +1,25 @@
-export default GifList;
+import React from 'react';
+import gifList from '/components/gifList';
+import Gif from '/components/Gif';
+
+
+const gifList = (props) => {
+ const  {gifs } = props;
+
+ return (
+    <div id='gifList'>
+
+        {gifs.map(gif => {
+         return (<Gif gif={gif} />)
+        })}
+    </div>
+ )
+}
+
+
+
+
+
+
+
+export default gifList;
